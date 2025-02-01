69345bc389f543ea0b17dd4ad9c182942850ed4a
pl1000100 <olszynski97@gmail.com>
2025-02-01 20:03:09 +0100

    Add tests, creating dirs and moving files

--- filesorter/sorter.py
+++ filesorter/sorter.py
@@ -7,9 +7,30 @@ class Sorter():
         self.source = None
         self.destination = None
         self.log = None
-        self.valid = False
         self.msg = None
+        self.valid = False
+
+        self.files_to_move = []
         self._handle_args(args)
+        if self.valid:
+            self.files_to_move = os.listdir(self.source)
+            self._move_files()
+    
+    def _move_files(self):
+        if not os.path.exists(self.destination):
+            os.mkdir(self.destination)
+        for item in self.files_to_move[:]:
+            file_path_source = os.path.join(self.source, item)
+            file_path_destination = os.path.join(self.destination, item)
+            if os.path.isfile(file_path_source) and len(item.split(".")) > 1:
+                file_extension = f"#{item.split(".")[-1]}"
+                file_path_extension_destination = os.path.join(self.destination, file_extension)
+                os.mkdir(file_path_extension_destination)
+                os.system(f"mv -v '{file_path_source}' '{os.path.join(file_path_extension_destination, item)}'")
+                continue
+            os.system(f"mv -v '{file_path_source}' '{self.destination}'")
+
+
 
     def _handle_args(self, args):
         if len(args) < 2 and ("-h" not in args and "--help" not in args):
@@ -31,7 +52,9 @@ class Sorter():
             self.msg = "Source not valid"
             return
         self.valid = True
+        self._handle_flags(args)
 
+    def _handle_flags(self, args):
         for flag in args[:-2]:
             f = flag.split("=", 1)
             flag_name = f[0]
